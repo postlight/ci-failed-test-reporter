f129ce7177584287ef37628f218adbfe3edd283d
Frankie Simms <fdsimms@gmail.com>
2019-03-07 12:07:25 -0500

    feat: add circleci support

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ const getTestReport = require('./getTestReport');
 const stripAnsi = require('strip-ansi');
 
 function comment(filepath: string) {
+  const username = process.env.CIRCLE_PR_USERNAME || process.env.PR_USERNAME;
+  const repoName = process.env.CIRCLE_PR_REPONAME || process.env.PR_REPONAME;
+  const prNumber = process.env.CIRCLE_PR_NUMBER || process.env.PR_NUMBER;
+
   const request: any = {
     method: 'POST',
     body: JSON.stringify({ body: stripAnsi(getTestReport(filepath)) }),
@@ -12,19 +16,18 @@ function comment(filepath: string) {
       'Content-Type': 'application/json',
       Accept: 'application/vnd.github.v3+json',
       Authorization: `token ${process.env.GITHUB_API_KEY}`,
-      'User-Agent': process.env.GITHUB_USERNAME
+      'User-Agent': username
     }
   };
-  
+
   nodeFetch(
-    'https://api.github.com/repos/fdsimms/timestack/issues/1/comments',
+    `https://api.github.com/repos/${username}/${repoName}/issues/${prNumber}/comments`,
     request
   ).then((res: any) => { console.log(res)});
-  return 'Okay';
 }
 
 function getReport(filepath: string) {
-  return filepath;
+  return stripAnsi(getTestReport(filepath));
 }
 
 module.exports = {
