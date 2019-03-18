a2ec95d50ffba57c1122abc991cb495f0bcda3a7
Frankie Simms <fdsimms@gmail.com>
2019-03-18 13:29:12 -0400

    wip

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,11 +1,23 @@
 import { getReport } from '../index';
-import sampleReport from './sample-report';
 
-test('getReport: returns formatted test report', () => {
-  // don't test the whitespace formatting, just the content
-  const testReport = getReport(__dirname + '/test-output.test.json').replace(
-    /\s/g,
-    ''
-  );
-  expect(testReport).toBe(sampleReport.replace(/[\s]*/g, ''));
+describe('getReport', () =>
+  test('returns formatted test report', () => {
+    // don't test the whitespace formatting, just the content
+    const testReport = getReport(__dirname + '/test-output.test.json').replace(
+      /\s/g,
+      ''
+    );
+    expect(testReport.startsWith('<details>')).toBeTruthy();
+  }));
+
+const multiplier = (num1, num2) => num1 * num2;
+
+describe('multiplier', () => {
+  test('multiplies two numbers', () => {
+    expect(multiplier(10, 10)).toEqual(100);
+  });
+
+  test('multiplies three numbers', () => {
+    expect(multiplier(10, 10, 10)).toEqual(1000);
+  });
 });
