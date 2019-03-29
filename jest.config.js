e8a963ad82805ebab8c205a3f474be1ed8a86a87
Frankie Simms <fdsimms@gmail.com>
2019-03-29 14:49:32 -0400

    fix: various documentation and testing issues (#7)

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  roots: ['./src'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
